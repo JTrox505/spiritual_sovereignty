2a53fbe96f91aa80e343742e82e9376010470527
JTrox505 <jacksynt@gmail.com>
2024-11-03 17:52:03 -0700

    cleaned up the formatting of the intention types and added three intentions

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 
-let LoveAndHappiness = ["Love And Happiness", "love and happiness are one of the highest vibrations that you can feel while in your physical body. This selection of crystals will help you open and heal your heart to feel unconditional love towards yourself, others, and lif This selection of crystals will help you open and heal your heart to feel unconditional love towards yourself, others, and life", "Shop Love And Happiness", "#faf7fa", "#fdf3fb", "#f8ccef", "./product_images/love_image"];
 
+let LoveAndHappiness = {
+	type: "Love And Happiness",
+	description: "love and happiness are one of the highest vibrations that you can feel while in your physical body. This selection of crystals will help you open and heal your heart to feel unconditional love towards yourself, others, and lif This selection of crystals will help you open and heal your heart to feel unconditional love towards yourself, others, and life",
+	shop: "Shop Love And Happiness",
+	left_side_background: "#faf7fa",
+	main_background: "#fdf3fb",
+	image_background: "#f8ccef",
+	image: "./product_images/love_image"
+};
+
+let BalanceAndWellness = {
+	type: "Balance And Wellnesss",
+	description: "Life is all about balance in order to achieve optimal health and wellness. This collection of crystals will assist you in balancing your yin and yang, chakras, and guidance to achieve feeling whole.",
+	shop: "Shop Balance And Wellness",
+	left_side_background: "#f4f5fa",
+	main_background: "#e6eafd",
+	image_background: "#99abf2",
+	image: "./product_images/balance_image"
+};
+
+let IntuitionAndSpirituality = {
+	type: "Intuition And Spirituality",
+	description: "Through any part of your spiritual journey, this selection of crystals will help you awaken your intuition, spark creativity, ignite a spiritual awakening, and deepen your connection with your spiritual guides.",
+	shop: "Shop Intuition And Spirituality",
+	left_side_background: "#f7f5f8",
+	main_background: "#f1eaf6",
+	image_background: "#c4aad7",
+	image: "./product_images/intuition_image"
+};
+
+let ManifestationAndAbundance {
+	type: "Manifestation And Abundance",
+	description: "When manifesting and attracting abundance into our lives, we must raise our vibrations, ask the universe, and let go of any limiting beliefs we have tied to our greatest desires. These crystals will assist you in manifesting and attracting more abundance at a faster rate.",
+	shop: "Shop Manifestation And Abundance",
+	left_side_background: "#f4f9f4",
+	main_background: "#e6f7e6",
+	image_background: "#9bdd99",
+	image: "./product_images/manifestation_image"
+};
 
 
 
@@ -19,12 +57,13 @@ const intention_image_container = document.querySelector(".intention_image_conta
 const intention_image = document.querySelector(".intention_image");
 
 intention_description.addEventListener("click", ()=> {
-	intention_type.textContent = LoveAndHappiness[0];
-	intention_description.textContent = LoveAndHappiness[1];
-	shop_intention.textContent = LoveAndHappiness[2];
+	intention_type.textContent = LoveAndHappiness.type;
+	intention_description.textContent = LoveAndHappiness.description;
+	shop_intention.textContent = LoveAndHappiness.shop;
 
-	left_side_intention.style.background = LoveAndHappiness[3];
-	intention_container.style.background = LoveAndHappiness[4];
-	intention_image_container.style.background = LoveAndHappiness[5];
-	intention_image.src = LoveAndHappiness[6];
+
+	left_side_intention.style.background = LoveAndHappiness.left_side_background;
+	intention_container.style.background = LoveAndHappiness.main_background;
+	intention_image_container.style.background = LoveAndHappiness.image_background;
+	intention_image.src = LoveAndHappiness.image;
 });
