529d7fb676f5725948dc1ce86f814ff525fca41a
JTrox <jacksynt@gmail.com>
2025-01-01 12:18:34 -0700

    added animation to collection filter section

--- shop/script.js
+++ shop/script.js
@@ -14,3 +14,34 @@ document.addEventListener("click", e => {
 		dropdown.classList.remove("active");
 	})
 })
+
+
+const COLLECTION_TITLE = document.querySelector(".collection-title");
+const COLLECTION = document.querySelector(".collection-container");
+
+
+let collectionText = document.querySelector(".collection-text");
+let expanded = document.querySelectorAll("expanded");
+console.log(expanded);
+
+let collectionHeight = COLLECTION.offsetHeight;
+COLLECTION_TITLE.addEventListener("click", () => {
+	
+	COLLECTION_TITLE.classList.toggle("expanded");
+
+	if(COLLECTION_TITLE.classList.contains("expanded")) {
+		collectionText.style.maxHeight = `${collectionText.scrollHeight}px`
+	}
+	else {
+		collectionText.style.maxHeight = "0px"; 
+	}
+})
+/*
+COLLECTION.forEach(COLLECTION => {
+	console.log(COLLECTION);
+	COLLECTION.addEventListener("click", () => {
+		COLLECTION.classList.toggle("expanded");
+	})
+
+})
+*/
