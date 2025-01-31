4fb220e30af5a0dc5170ffd9c4f50ad4d8f7de06
JTrox <jacksynt@gmail.com>
2025-01-31 16:18:49 -0700

    added the content for the color filter and changed the js filter button function to be able to apply to all filter buttons rather than having to have an event listener for every filter button.

--- shop/script.js
+++ shop/script.js
@@ -16,19 +16,32 @@ document.addEventListener("click", e => {
 })
 
 
-const COLLECTION_TITLE = document.querySelector(".collection-title");
-const COLLECTION_CONTAINER = document.querySelector(".collection-container");
+document.addEventListener("click", e => {
+	const isFilterDropdown = e.target.matches("[data-filter-dropdown-button]");
+	if(!isFilterDropdown && e.target.closest("[data-filter-dropdown]") !=null) return;
 
+	let currentFilter;
+	let currentText;
+	if(isFilterDropdown) {
+		currentFilter = e.target.closest("[data-filter-dropdown-button]");
+		currentFilter.classList.toggle("expanded");
 
-const COLLECTION_TEXT = document.querySelector(".collection-text");
+		currentText = currentFilter.nextElementSibling;
+		if(currentFilter.classList.contains("expanded")) {
+			currentText.style.maxHeight = `${currentText.scrollHeight}px`;
+		}
+		else {
+			currentText.style.maxHeight = "0px";
+		}
 
-let collectionHeight = COLLECTION_CONTAINER.offsetHeight;
-COLLECTION_TITLE.addEventListener("click", () => {
-	
-	COLLECTION_TITLE.classList.toggle("expanded");
+	}
 
-	if(COLLECTION_TITLE.classList.contains("expanded")) {
-		return COLLECTION_TEXT.style.maxHeight = `${COLLECTION_TEXT.scrollHeight}px`
-	}		
-	COLLECTION_TEXT.style.maxHeight = "0px"; 
+	document.querySelectorAll("[data-filter-dropdown-button].expanded").forEach(filterDropdown => {
+		if(filterDropdown === currentFilter) return;
+//This is implemented because I only want a filter dropdown to reset if another one is expanded. 
+		else if(isFilterDropdown) {
+			filterDropdown.classList.remove("expanded");
+			filterDropdown.nextElementSibling.style.maxHeight = "0px";
+		}
+	})
 })
